90d87a99cbddf7f15c740712cfe253b67c6c0bdb
Lexa <lexaraea@gmail.com>
2020-05-11 16:00:35 -0400

    Add api

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ window.addEventListener('load', () => {
        navigator.geolocation.getCurrentPosition(position => {
            long = position.coords.longitude;
            lat = position.coords.latitude;
-       });
+
+           const proxy = 'http://cors-anywhere.herokuapp.com/';
+           const api = `https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139`;
+
+           fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
+        });
    }
 });
